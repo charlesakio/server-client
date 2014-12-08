91bc860ef4ca0c477d3879983f0e27dfb84e9d8f
Charles Javelona <charlesjavelona@gmail.com>
2014-12-08 12:04:41 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 #Client code
 from socket import *
 
-serverip = '' #****Use your server IP address here****
+serverip = '142.55.107.217' #Use your server IP address here
 serverport = 5555 #Use last 4 digits of your ID
 datasize = 1024 #Note the double parenthesis (why?)
 
@@ -10,13 +10,19 @@ sock1.connect((serverip,serverport))
 close_flag = False
 
 while not close_flag:
-    data_Out = input('Enter some text here.. ')
+    data_Out = input('Enter file name ')
     if len(data_Out):
         sock1.send(data_Out.encode())
     if data_Out == 'end' or data_Out == 'quit':
         close_flag=True
     else:
         dataIn=sock1.recv(datasize)
-        print('Received data from server: ',dataIn.decode())
-        print('Closing connection with server', serverip)
-        sock1.close()
+        print('Received data from server: \n', dataIn.decode())
+        download_file = open(data_Out, 'w') # Creates file
+        file_input = dataIn.decode()
+        download_file.write(file_input)
+        download_file.close()
+
+        print('File saved ' + data_Out)
+print('Closing connection with server', serverip)
+sock1.close()
